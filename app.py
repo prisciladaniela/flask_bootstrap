22c1fa31f961c98641ead505f4acaf9802329351
Priscila <prisciladaniela25@hotmail.com>
2022-11-09 11:03:34 -0300

    flask temple

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask import request
+from flask import render_template
 
 app = Flask(__name__)
 
@@ -6,3 +8,17 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return 'Hola Mundo'
+
+@app.route('/usuario/<name>')
+def user(name):
+    return render_template("user.html", name = name)
+
+@app.route('/navegador')
+def browser():
+    user_agent = request.headers.get('User-Agent')
+    return f'Tu navegador es:{user_agent}'
+
+@app.route('/rutas')
+def routes():
+    print(app.url_map)
+    return"revisa tu consola para ver las rutas"
