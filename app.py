834a6f85f15ba76d238420e3427d287191382c13
Priscila <prisciladaniela25@hotmail.com>
2022-11-12 10:30:31 -0300

    templates

--- app.py
+++ app.py
@@ -1,24 +1,48 @@
-from flask import Flask
-from flask import request
-from flask import render_template
+from flask import Flask, request, render_template 
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return 'Hola Mundo'
+def hello_world():
+  return render_template('index.html')
+
 
 @app.route('/usuario/<name>')
 def user(name):
-    return render_template("user.html", name = name)
+  return render_template('user.html', name = name)
+
+@app.route('/usuario')
+def stranger():
+  return render_template('user.html')
+
 
 @app.route('/navegador')
 def browser():
-    user_agent = request.headers.get('User-Agent')
-    return f'Tu navegador es:{user_agent}'
+  user_agent = request.headers.get('User-Agent')
+  return f'Tu navegafor es: {user_agent}'
 
 @app.route('/rutas')
 def routes():
-    print(app.url_map)
-    return"revisa tu consola para ver las rutas"
+  
+  print(app.url_map)
+  return 'revisa tu consola para ver las rutas'
+
+@app.errorhandler(404)
+def page_not_found(error):
+  return render_template('page_not_found.html'), 400
+
+
+
+
+
+
+
+
+
+
+
+
+
+
