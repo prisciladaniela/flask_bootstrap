6dbe34a20ec92d2e8fc0519f83dfd5b60340e5b4
Priscila <prisciladaniela25@hotmail.com>
2022-12-05 11:11:04 -0300

    inicio de sesion

--- app/extensions.py
+++ app/extensions.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+flask flask_login import LoginManager
 
 db =SQLAlchemy()
 migrate = Migrate()
--- app/memes/routes.py
+++ app/memes/routes.py
@@ -1,8 +1,10 @@
 from app.memes import bp
 from flask import render_template
+from flask login import login_required
 
 
 
 @bp.route('/')
+@login.required
 def index():
     return render_template('memes/index.html')
